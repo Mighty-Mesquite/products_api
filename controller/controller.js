d39207e3d9abcfbafa59d6560cc20210c50bcfe5
Dana Walsh <dana.e.walsh@gmail.com>
2021-03-16 15:32:10 -0700

    Implementing db queries as best I can without data

--- controller/controller.js
+++ controller/controller.js
@@ -1,8 +1,30 @@
 
 module.exports = {
   getProducts: (request, response) => {
-    console.log('Im in getProducts');
+
     response.send('Done')
+    // check to see if there is a specific count and page on the request
+    if (Object.keys(request.query).length > 0) {
+      if (request.query.page && request.query.count) {
+        console.log('There is a specified page AND count')
+      } else if (request.query.count) {
+        console.log('There is a specific count')
+      } else if (request.query.page) {
+        console.log('There is a specified page')
+      }
+    } else {
+      // no specific
+      db.query(`SELECT * FROM products LIMIT 5`, (error, data) => {
+        if (error) {
+          console.log('ERROR', error);
+          response.send(error);
+        } else {
+          // need to check data structure
+          console.log(data);
+          // response.status(200).send(data)
+        }
+      })
+    }
   },
   getOneProduct: (request, response) => {
     console.log('Im in getOneProduct')
@@ -14,6 +36,17 @@ module.exports = {
   },
   getRelatedProducts: (request, response) => {
     console.log('getting related products')
+    const { product_id } = request.params;
+    db.query(`SELECT * FROM related WHERE product1_id=${product_id}`, (error, data) => {
+      if (error) {
+        console.log('ERROR fetching related products', error);
+        response.send(error);
+      } else {
+        // check data structure
+        console.log(data);
+        // response.status(200).send(data)
+      }
+    })
     response.send('Done')
   }
 }
